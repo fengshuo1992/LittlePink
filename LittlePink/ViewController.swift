7a1368e463f7808b03b6075a41b8e2d0b0f1ee6f
fengshuo_2 <963787902@qq.com>
2021-05-07 20:42:52 +0800

    pod

--- LittlePink/ViewController.swift
+++ LittlePink/ViewController.swift
@@ -6,11 +6,21 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    lazy var button = UIButton.init(type: .custom)
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(button)
+        button.snp.makeConstraints { (make) in
+            make.height.equalTo(50)
+            make.width.equalTo(50)
+            make.centerX.equalTo(self.view)
+            make.centerY.equalTo(self.view)
+        }
+      
+        button.backgroundColor = .red
     }
 
 
